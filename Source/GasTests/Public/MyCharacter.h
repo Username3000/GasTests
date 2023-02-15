aacf03e837190c94d3c3eb1a8b0af6f30e28b347
Username3000 <47796487+Username3000@users.noreply.github.com>
2023-02-16 01:44:06 +0300

    Добавляю GAS в проект
    *Сделал базового перса с GAS
    *Перенаследовал блюпринтового перса от перса с GAS
    *Сделал кастомный компонент GAS
    
    *Играюсь с гит игнором

---
+++ Source/GasTests/Public/MyCharacter.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "GameplayEffectTypes.h"
+#include "MyAbilitySystemComponent.h"
+#include "GameplayEffectTypes.h"
+
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class GASTESTS_API AMyCharacter : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UMyAbilitySystemComponent* AbilitySystemComponent;
+
+
+public:
+	// Sets default values for this character's properties
+	AMyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	//AbilitySystemComponent getr
+	virtual class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+};
