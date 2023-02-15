4782f2d04c196128ff93ccb2c29b4a6d8fb8b0b3
Username3000 <47796487+Username3000@users.noreply.github.com>
2023-02-16 00:53:05 +0300

    Заливаю пустой проект с сурсами

---
+++ Source/GasTests/GasTests.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GasTests.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GasTests, "GasTests" );
