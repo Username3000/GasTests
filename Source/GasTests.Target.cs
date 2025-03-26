37313e5bd78e64991c5eb5aa104c4015e9f58f9b
Username3000 <47796487+Username3000@users.noreply.github.com>
2025-03-26 19:37:56 +0300

    Обновил до 5.5

--- Source/GasTests.Target.cs
+++ Source/GasTests.Target.cs
@@ -8,7 +8,7 @@ public class GasTestsTarget : TargetRules
 	public GasTestsTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "GasTests" } );
 	}
